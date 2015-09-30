d26021c493f5af60b639d7c965bed68f1f87d45c
VeterAlex <twinzata@gmail.com>
2015-09-30 10:55:58 +0300

    Initial commit

---
+++ src/Task1Number6_1.java
@@ -0,0 +1,15 @@
+
+public class Task1Number6_1 {
+
+    public static void main(String[] args) {
+
+        for (int n = 0; n < 100; n = n + 2) {
+     // Выведет четные числа от 0 до 100. Если переменную n = 1, то выведет нечетные числа
+        System.out.println(n);
+
+    }
+
+        }
+    }
+
+        
